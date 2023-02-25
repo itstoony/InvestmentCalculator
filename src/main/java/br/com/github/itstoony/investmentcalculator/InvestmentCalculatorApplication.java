71c3e40b8a5c4c9996b55c90400f66624329bd82
tony <toonyrenner@gmail.com>
2023-02-25 13:51:42 -0300

    Creating project

---
+++ src/main/java/br/com/github/itstoony/investmentcalculator/InvestmentCalculatorApplication.java
@@ -0,0 +1,13 @@
+package br.com.github.itstoony.investmentcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvestmentCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/github/itstoony/investmentcalculator/InvestmentCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.github.itstoony.investmentcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
